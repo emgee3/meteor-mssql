404ce6a5e2065bd4591864fba43ba94b6e729d78
emgee3 <hello@gravitronic.com>
2016-08-02 20:32:00 -0700

    Update upstream

--- package.js
+++ package.js
@@ -1,16 +1,15 @@
 Package.describe({
   name: "emgee:mssql",
   summary: "mssql wrapper: non-reactive SQL Server package",
-  version: "1.3.0_8",
+  version: "3.3.0",
   git: "https://github.com/emgee3/meteor-mssql.git",
   documentation: "README.md"
 });
 
-Npm.depends({ "mssql" : "1.3.0" });
+Npm.depends({ "mssql" : "3.3.0" });
 
 Package.onUse(function(api) {
-  api.versionsFrom("1.0.3.1");
-  api.versionsFrom("1.1.0.2");
+  api.versionsFrom("1.4.0.1");
   api.use('underscore', 'server');
   api.addFiles("mssql.js", "server");
   api.export("Sql", "server");
